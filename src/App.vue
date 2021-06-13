56f1076442995a2d320309a87beacd133c9e1b9c
leticiakosasih <leticiakosasih00@gmail.com>
2021-06-14 00:56:42 +0700

    nambah router, tugas 4 part 3 done

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <Todo />
-  <User />
+  <router-link to="/todo">Halaman Todo</router-link>
+  &nbsp;
+  <router-link to="/user">Halaman User</router-link>
+  <router-view></router-view>
 </template>
 
 <script setup>
